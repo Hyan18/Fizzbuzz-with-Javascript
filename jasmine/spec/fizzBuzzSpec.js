fd3c293b99edb4493d32c6cbc42e0595c3bd05a4
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-12-09 17:18:15 +0000

    Complete fizzbuzz javascript challenge

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -3,15 +3,19 @@ describe("fizzBuzz", function() {
     expect(fizzBuzz(1)).toEqual(1);
   });
 
-  it("should return 'fizz' when given 3", function() {
+  it("should return 'Fizz' when given 3", function() {
     expect(fizzBuzz(3)).toEqual('Fizz');
   });
 
-  it("should return 'buzz' when given 5", function() {
+  it("should return 'Buzz' when given 5", function() {
     expect(fizzBuzz(5)).toEqual('Buzz');
   });
 
   it("should return 'FizzBuzz' when given 15", function() {
     expect(fizzBuzz(15)).toEqual('FizzBuzz');
   });
+
+  it("should return 'Buzz' when given 10", function() {
+    expect(fizzBuzz(10)).toEqual('Buzz');
+  })
 });
